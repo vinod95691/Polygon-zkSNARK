f5fceafca184d2861e98a2502fb834cc3061a29d
vinod95691 <vinodyogi95691@gmail.com>
2024-06-22 00:23:58 +0530

    Add files via upload

---
+++ circuits/multiplier/circuit.circom
@@ -0,0 +1,53 @@
+pragma circom 2.0.0;
+
+template GivenCircuit () {  
+
+   signal input a;
+   signal input b;
+   signal X;
+   signal Y;
+   signal output Q;
+
+   // Component gates used to create custom curcuit
+   component andGate = AND();
+   component notGate = NOT();
+   component orGate = OR();
+
+   // Circuit Logic
+   andGate.a <== a;
+   andGate.b <== b;
+   X <== andGate.out;
+
+   notGate.in <== b;
+   Y <== notGate.out;
+
+   orGate.a <== X;
+   orGate.b <== Y;
+   Q <== orGate.out;
+
+}
+
+template AND() {
+    signal input a;
+    signal input b;
+    signal output out;
+
+    out <== a*b;
+}
+
+template NOT() {
+    signal input in;
+    signal output out;
+
+    out <== 1 + in - 2*in;
+}
+
+template OR() {
+    signal input a;
+    signal input b;
+    signal output out;
+
+    out <== a + b - a*b;
+}
+
+component main = GivenCircuit();
---
+++ hardhat.config.ts
@@ -0,0 +1,47 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+import "hardhat-circom";
+// circuits
+import circuits = require('./circuits.config.json')
+import 'dotenv/config'
+// set env var to the root of the project
+//const INFURA_API_KEY = "KEY";
+//const SEPOLIA_PRIVATE_KEY = "YOUR SEPOLIA PRIVATE KEY";
+process.env.BASE_PATH = __dirname;
+import 'dotenv/config';
+// tasks
+import "./tasks/newcircuit.ts"
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17",
+      },
+      {
+        version: "0.6.11",
+      }
+    ]
+  },
+  networks:{
+    mumbai: {
+      url: 'https://rpc.ankr.com/polygon_mumbai',
+      accounts: [process.env.privatekey]
+    },
+  },
+   
+  
+  
+
+  circom: {
+    // (optional) Base path for input files, defaults to `./circuits/`
+    inputBasePath: "./circuits",
+    // (required) The final ptau file, relative to inputBasePath, from a Phase 1 ceremony
+    ptau: "powersOfTau28_hez_final_12.ptau",
+    // (required) Each object in this array refers to a separate circuit
+    circuits: JSON.parse(JSON.stringify(circuits))
+  },
+};
+
+export default config;
